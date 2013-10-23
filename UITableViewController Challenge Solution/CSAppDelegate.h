2c27408fe038be9abf48bf53fdbcfd72c1b7a979
Gustavo Campeol <gustavocampeol@MacBook-Pro-de-Gustavo-Campeol.local>
2013-10-22 22:46:29 -0200

    Initial Commit

---
+++ UITableViewController Challenge Solution/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Gustavo Campeol on 22/10/13.
+//  Copyright (c) 2013 Gustavo Campeol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CSViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Gustavo Campeol on 22/10/13.
+//  Copyright (c) 2013 Gustavo Campeol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSViewController : UIViewController
+
+@end
